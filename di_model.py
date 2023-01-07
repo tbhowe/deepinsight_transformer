2decbe2cbee131d45fe20ebc534402f890bdcb67
Tim Howe <53109226+tbhowe@users.noreply.github.com>
2023-01-07 12:30:00 +0000

    renames model.py > di_model.py

---
+++ di_model.py
@@ -0,0 +1,41 @@
+#%%
+import torch
+from transformers import ViTModel, ViTImageProcessor, ViTConfig
+import os
+import time
+import torch.nn.functional as F
+
+
+class DeepInsightVitModel(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.model_path='google/vit-base-patch16-224-in21k'
+        self.layers = ViTModel.from_pretrained(self.model_path)
+        for param in self.layers.parameters():
+            param.grad_required = False  
+        self.regression_head_1=torch.nn.Linear(in_features= 768, out_features= 1)
+        self.regression_head_2=torch.nn.Linear(in_features= 768, out_features= 1)
+        self.layers.regression1 = self.regression_head_1
+        self.layers.regression2 = self.regression_head_2
+
+    def forward(self, x):
+        output=self.layers(x)
+        pooler_output=output.pooler_output
+        out1 = self.layers.regression1(pooler_output)
+        out2 = self.layers.regression2(pooler_output)
+        return out1,out2
+    
+    @staticmethod
+    def combined_loss(out1, out2, labels):
+        loss_fn1 = F.mse_loss()
+        loss_fn2 = F.mse_loss()
+        loss1 = loss_fn1(out1, labels[0])
+        loss2 = loss_fn2(out2, labels[1])
+        return loss1 + loss2
+        
+if __name__ == "__main__":
+    model=DeepInsightVitModel()
+    model.layers()
+
+# %%
