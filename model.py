9ca9f2f1b4ee63979f854854a9cce164ae6dac06
Tim Howe <53109226+tbhowe@users.noreply.github.com>
2023-01-07 09:02:29 +0000

    corrects in_features size for regression heads, sets output to pooler_output

--- model.py
+++ model.py
@@ -12,13 +12,14 @@ class DeepInsightVitModel(torch.nn.Module):
         self.layers = ViTModel.from_pretrained(self.model_path)
         for param in self.layers.parameters():
             param.grad_required = False  
-        self.regression_head_1=torch.nn.Linear(in_features= 20, out_features= 1)
-        self.regression_head_2=torch.nn.Linear(in_features= 20, out_features= 1)
+        self.regression_head_1=torch.nn.Linear(in_features= 768, out_features= 1)
+        self.regression_head_2=torch.nn.Linear(in_features= 768, out_features= 1)
         
     def forward(self, x):
         output=self.layers(x)
-        out1 = self.head1(output)
-        out2 = self.head2(output)
+        pooler_output=output.pooler_output
+        out1 = self.head1(pooler_output)
+        out2 = self.head2(pooler_output)
         return out1,out2
         
 if __name__ == "__main__":
