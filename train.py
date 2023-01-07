cfaa7fe96a4bb5cfbd4d6a67c24dd8e4cfe98b5f
Tim Howe <53109226+tbhowe@users.noreply.github.com>
2023-01-07 09:41:11 +0000

    Creates dataloaders in train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-
+#%%
 from di_dataset import DeepInsightDataset
 from model import DeepInsightVitModel
 from torch.utils.data import random_split
@@ -6,18 +6,30 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torch.optim import lr_scheduler
-from transformers import ViTImageProcessor
 import torch
 
 
-# TODO - set up transform - take PIL image, return 
-# ImageProcessor output and labels in correct input format  - dict?
-
 # TODO - init dataloaders + do train/val/test split
 
+dataset = DeepInsightDataset()
+train_set_len = round(0.8*len(dataset))
+val_set_len = round(0.1*len(dataset))
+test_set_len = len(dataset) - val_set_len - train_set_len
+split_lengths = [train_set_len, val_set_len, test_set_len]
+# split the data to get validation and test sets
+train_set, val_set, test_set = random_split(dataset, split_lengths)
+
+batch_size = 16
+train_loader = DataLoader(train_set, shuffle=True, batch_size=batch_size)
+val_loader = DataLoader(val_set, batch_size=batch_size)
+test_loader = DataLoader(test_set, batch_size=batch_size)
+
 # TODO - init optimiser for FC layer - remember two regression heads!
+
 
 # TODO - define train loop
 
 # TODO  - define eval function
 
+
+# %%
